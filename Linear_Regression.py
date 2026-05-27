3d6a8a8ac3668017560244b0e61e6e06f31f602b
Shubh Mishra <shubhmishra7482@gmail.com>
2026-05-27 19:52:02 +0530

    feat: implement linear regression using gradient descent

---
+++ Linear_Regression.py
@@ -0,0 +1,29 @@
+#LINEAR REGRESSION USING GRADIENT
+
+import numpy as np
+class LinearRegression:
+    def __init__ (self,learning_rate = 0.01,iterations = 1000):
+        self.lr = learning_rate
+        self.iterations = iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self,X,y):
+        samples , features = X.shape
+        self.weights = np.zeros(features)
+        self.bias = 0
+        for i in range (self.iterations):
+            y_predicted = np.dot(X, self.weights) + self.bias
+
+            dw = 1/samples * np.dot(X.T ,(y_predicted - y))
+            db = 1/samples * np.sum( y_predicted - y)
+
+            self.weights = self.weights - self.lr *dw
+            self.bias = self.bias - self.lr *db
+
+    def predict ( self , X) :
+            y_predicted = np.dot(X, self.weights)  + self.bias
+            return y_predicted;
+
+
+
